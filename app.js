bf7ee77d13b6cfed9465d764e9071f75febb3dda
Mahedi hasan <mahedi2112p@gmail.com>
2022-02-17 11:40:32 +0600

    balance added

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 document.getElementById('calculate').addEventListener('click', function() {
-    console.log('Congratulation Mahedi');
+    //income value
+    const income = document.getElementById('income');
+    totalIncome = parseFloat(income.value);
+    //expenses value
+    const food = document.getElementById('food-cost');
+    foodResult = parseFloat(food.value);
+
+    const rent = document.getElementById('rent-cost');
+    rentResult = parseFloat(rent.value);
+
+    const colth = document.getElementById('cloth-cost');
+    clothResult = parseFloat(colth.value);
+    //total expens
+    const totalCost = foodResult + rentResult + clothResult;
+    console.log(totalCost);
+    const expens = document.getElementById('total-expens');
+    expens.innerText = totalCost;
+
+    const finalBalance = totalIncome - totalCost;
+    const balance = document.getElementById('final-balance');
+    balance.innerText = parseFloat(finalBalance);
+
 });
