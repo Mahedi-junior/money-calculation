622b72b14d406ec9b7d2973e2993652313dc2179
Mahedi hasan <mahedi2112p@gmail.com>
2022-02-17 15:26:38 +0600

    saving calulation added

--- app.js
+++ app.js
@@ -16,9 +16,25 @@ document.getElementById('calculate').addEventListener('click', function() {
     console.log(totalCost);
     const expens = document.getElementById('total-expens');
     expens.innerText = totalCost;
-
+    //current balance
     const finalBalance = totalIncome - totalCost;
     const balance = document.getElementById('final-balance');
     balance.innerText = parseFloat(finalBalance);
-
 });
+
+//saving calculation
+document.getElementById('save-btn').addEventListener('click', function() {
+    const currentbalance = parseFloat(document.getElementById('final-balance').innerText);
+    //saving balance
+    const saveInput = document.getElementById('save-input').value;
+    const saving = currentbalance * (saveInput / 100);
+    //saving amount
+    savingText = document.getElementById('saving');
+    savingText.innerText = parseInt(saving);
+    const remainigbalance = currentbalance - saving;
+    console.log(remainigbalance);
+    const remainigText = document.getElementById('remaining-blance');
+    remainigText.innerText = parseFloat(remainigbalance);
+
+
+})
